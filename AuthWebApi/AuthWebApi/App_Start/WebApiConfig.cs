b3dd714c9476c69ddafe7579f36f57768ef475ad
Gio <gio.garciap@gmail.com>
2019-03-09 13:29:37 -0500

    add swagger doc with authentication bearer

--- AuthWebApi/AuthWebApi/App_Start/WebApiConfig.cs
+++ AuthWebApi/AuthWebApi/App_Start/WebApiConfig.cs
@@ -6,23 +6,23 @@ using System.Web.Http;
 
 namespace AuthWebApi
 {
-    public static class WebApiConfig
-    {
-        public static void Register(HttpConfiguration config)
-        {
-            // Web API configuration and services
+   public static class WebApiConfig
+   {
+      public static void Register(HttpConfiguration config)
+      {
+         // Web API configuration and services
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
+         // Web API routes
+         config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-
+         config.Routes.MapHttpRoute(
+             name: "DefaultApi",
+             routeTemplate: "api/{controller}/{id}",
+             defaults: new { id = RouteParameter.Optional }
+         );
+         SwaggerConfig.Register(config);
          var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First(); // forza el formato json a CamelCasePropertyNamesContractResolver
 
       }
-    }
+   }
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # token-based-auth-api
+
+Adds Swagger documentation 
+https://evgenyzborovsky.com/2017/02/03/adding-swagger-to-asp-net-web-api/
